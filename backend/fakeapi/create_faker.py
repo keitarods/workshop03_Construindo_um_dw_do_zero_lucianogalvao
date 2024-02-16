2b1534938c6bfb39d4ecc793585d00bd3adc5a7b
keitarods <keitarojp@hotmail.com>
2024-02-15 23:14:02 -0300

    Inserida função na etapa de ETL que verifica se existe o arquivo products.csv e cria

--- backend/fakeapi/create_faker.py
+++ backend/fakeapi/create_faker.py
@@ -1,8 +1,10 @@
 import faker as fk
+import faker_commerce
+import os
 from faker import Faker
 from random import randint, uniform
 from pandas import DataFrame
-import faker_commerce
+
 
 #Instanciando a classe Faker.
 fake = Faker()
@@ -10,18 +12,25 @@ fake = Faker()
 #Adicionando o provider com nomes de produtos.
 fake.add_provider(faker_commerce.Provider)
 
-list_product = []
-i = 0
 
+def criar_products(param: int):
 #Loop para criação de uma lista com 150 produtos aleatórios.
-while i <= 150:
-    ean = randint(100000000000000,999999999999999)
-    name_product = fake.ecommerce_name()
-    value = round(uniform(0, 100),2)
-    list_product.append([ean,name_product,value])
-    i += 1
+    list_product = []
+    i = 0
+    print(f"aquiiiiiiii {os.getcwd()}")
+    while i <= param:
+        ean = randint(100000000000000,999999999999999)
+        name_product = fake.ecommerce_name()
+        value = round(uniform(0, 100),2)
+        list_product.append([ean,name_product,value])
+        i += 1
 
-#Criando Dataframe dos produtos e gerando o arquivo products.csv.
-df = DataFrame(list_product, columns=["EAN","NAME","VALUE"])
+    #Criando Dataframe dos produtos e gerando o arquivo products.csv.
+    df = DataFrame(list_product, columns=["EAN","NAME","VALUE"])
 
-df.to_csv("backend/fakeapi/products.csv", index = False, lineterminator=None)
+    #Verifica se o arquivo products.csv ja existe
+    if not os.path.exists("../fakeapi/products.csv"):
+        df.to_csv("../fakeapi/products.csv", index = False, lineterminator=None)
+        print("Arquivo products.csv criado")
+    else:
+        print("O arquivo products.csv ja existe")
--- backend/start.py
+++ backend/start.py
@@ -1,6 +1,8 @@
 from datasource.api import APICollector
 from contracts.schema import Compraschema
+from fakeapi.create_faker import criar_products
 from drive.client import DriveClient
+
 
 import time
 import schedule
@@ -12,6 +14,9 @@ def apicollector(schema, drive, repeat):
     response = APICollector(schema, drive).start(repeat)
     print("Processo executado")
     return response
+
+
+criar_products(100)
 
 # Esta parte do código define um agendamento usando a biblioteca schedule. Ele especifica que a função apicollector deve ser executada a cada 1 minuto.
 schedule.every(1).minutes.do(apicollector, schema, drive, 50)
