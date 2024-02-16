a387b55d6350d5b8ac8b2ff93af0070ffd32765a
keitarods <keitarojp@hotmail.com>
2024-02-15 22:39:53 -0300

    Adicionado schedule para execução do código de tempo em tempo

--- backend/start.py
+++ backend/start.py
@@ -2,9 +2,21 @@ from datasource.api import APICollector
 from contracts.schema import Compraschema
 from drive.client import DriveClient
 
+import time
+import schedule
+
 schema = Compraschema
 drive = DriveClient()
 
-minha_classe = APICollector(schema, drive).start(3)
+def apicollector(schema, drive, repeat):
+    response = APICollector(schema, drive).start(repeat)
+    print("Processo executado")
+    return response
 
-print(minha_classe) 
+# Esta parte do código define um agendamento usando a biblioteca schedule. Ele especifica que a função apicollector deve ser executada a cada 1 minuto.
+schedule.every(1).minutes.do(apicollector, schema, drive, 50)
+
+#Loop que define enquanto for == True, o código será executado baseado no time setado no schedule
+while True:
+    schedule.run_pending()
+    time.sleep(1)
